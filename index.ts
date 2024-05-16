e0bf1c61bdb5a79db68f4c5afec5790eb74e759b
Lumi Pakkanen <lumi.pakkanen@gmail.com>
2024-05-16 20:09:13 +0300

    Add CORS headers for local development
    Call the filename part 'id'.

--- index.ts
+++ index.ts
@@ -32,59 +32,65 @@ stat(SCALE_PATH, (err, stats) => {
   }
 });
 
+function response(body: BodyInit, init?: ResponseInit) {
+  const res = new Response(body, init);
+  res.headers.set('Access-Control-Allow-Origin', 'http://localhost:5173');
+  return res;
+}
+
 const server = Bun.serve({
   async fetch(req) {
     const path = new URL(req.url).pathname;
 
     // Respond with text/html in development. Should not be accessible in production.
     if (path === '/') {
-      const headers = new Headers();
-      headers.append('Content-Type', 'text/html');
-      return new Response(INDEX_BODY, {headers});
+      const res = response(INDEX_BODY);
+      res.headers.append('Content-Type', 'text/html');
+      return res;
     }
 
     // Simple file-based system for storing scales.
     if (path === '/scale') {
       if (req.method !== 'POST') {
-        return new Response('Method not allowed', {status: 405});
+        return response('Method not allowed', {status: 405});
       }
       const data = await req.json();
       // TODO: Validate data
       // TODO: Generate preview image
-      const filename = join(SCALE_PATH, data.name + '.json');
+      const filename = join(SCALE_PATH, data.id + '.json');
       const file = Bun.file(filename);
       if (await file.exists()) {
-        return new Response('Scale already exists', {status: 409});
+        return response('Scale already exists', {status: 409});
       }
       await Bun.write(file, JSON.stringify(data.payload));
 
-      return new Response('Scale created', {status: 201});
+      return response('Scale created', {status: 201});
     }
 
     // Read a stored scale. This should be bypassed in production.
     if (path.startsWith('/scale/')) {
       const {dir, base, ext} = parse(path);
       if (dir !== '/scale') {
-        return new Response('Bad scale path', {status: 400});
+        return response('Bad scale path', {status: 400});
       }
       if (base.length > 255) {
-        return new Response('Scale name too long', {status: 414});
+        return response('Scale id too long', {status: 414});
       }
       if (ext !== '.json') {
-        return new Response('Not found', {status: 404});
+        return response('Not found', {status: 404});
       }
       const filename = join(SCALE_PATH, base);
       const file = Bun.file(filename);
-      return new Response(file);
+      return response(file);
     }
 
     // We don't brew coffee here.
     if (path === '/coffee') {
-      return new Response("I'm a teapot", {status: 418});
+      return response("I'm a teapot", {status: 418});
     }
 
     // 404s
-    return new Response('Page not found', {status: 404});
+    return response('Page not found', {status: 404});
   },
 });
 
